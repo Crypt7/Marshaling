6573dffb8292ee28fa9d20ca05d3b641251e8ae5
Рогожин Виталий Владимирович <Rogo_vv81>
2018-10-12 19:01:26 +0300

    Add project files.

---
+++ mainProject/NM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace mainProject
+{
+    internal static class NM
+    {
+        private const string DllName = @"..\..\..\Debug\Project1.dll";
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Add(int a, int b);
+    }
+}
---
+++ mainProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace mainProject
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 50;
+            int Sum = NM.Add(a, b);
+            Console.WriteLine(Sum);
+        }
+                     
+   
+    }
+}
